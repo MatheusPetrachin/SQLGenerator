42231a37bfb8fb6390ae461de29dc5a2eaee6791
MatheusPetrachin <mrp.petrachin@gmail.com>
2022-12-01 14:49:51 -0300

    first commit

---
+++ Temp/SQL.sql
@@ -0,0 +1,6 @@
+INSERT INTO table VALUES
+('a0d16d31-73b4-43fd-932d-ef3dae602e3e',2,'2','B','2016-01-01',NULL,'B',NULL,'Formula X','L',NULL),
+('59c0fb7f-7561-44fe-8f04-18ecd01cb1fd',2,'5','C','2017-01-01',NULL,'C',NULL,NULL,'R',NULL),
+('65bdfad0-74cc-4469-922a-3aea891c3d8b',2,'6','D','2018-01-01',NULL,'A',NULL,NULL,'A','Texto Qualquer'),
+('5a9d8466-a79d-4908-898e-d361a3201971',2,'7','B','2019-01-01',NULL,'B',NULL,NULL,'A','Texto Qualquer'),
+('0b589b27-f618-4ed2-9c75-d6761f4724f1',2,'8','C','2020-01-01',NULL,'C',NULL,NULL,'A','Texto Qualquer');
