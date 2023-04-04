29b033c0f5bcfd3f18002195592796afd66a6459
mpetrachin-qyon <matheus.petrachin@qyon.com>
2023-04-03 21:29:58 -0300

    dev

--- Temp/SQL.sql
+++ Temp/SQL.sql
@@ -1,6 +1,6 @@
-INSERT INTO table VALUES
-('47bfbb28-b0d2-40ae-ad7c-e5db82d556ba',2,'2','B','2016-01-01',NULL,'B',NULL,'Formula X','L',NULL),
-('74c2b99b-1ea9-4b7a-913f-0016dfc9e27a',2,'5','C','2017-01-01',NULL,'C',NULL,NULL,'R',NULL),
-('6496f90c-3502-4875-8545-68113554f360',2,'6','D','2018-01-01',NULL,'A',NULL,NULL,'A','Texto Qualquer'),
-('4254725a-6e0d-4064-9f52-083490909bea',2,'7','B','2019-01-01',NULL,'B',NULL,NULL,'A','Texto Qualquer'),
-('711aa245-7112-4787-9a2f-52701f56358d',2,'8','C','2020-01-01',NULL,'C',NULL,NULL,'A','Texto Qualquer');
+INSERT INTO users VALUES
+('e09a7b4d-b584-46a8-a542-7fa1219f729f',2,'2','B','2016-01-01',NULL,'B',NULL,'Formula X','L',NULL),
+('bf87d51a-3a6f-4e67-ad3e-bcb9e481f760',2,'5','C','2017-01-01',NULL,'C',NULL,NULL,'R',NULL),
+('a388bb17-90c7-47ac-91d2-5925ce1d157e',2,'6','D','2018-01-01',NULL,'A',NULL,NULL,'A','Texto Qualquer'),
+('bd61b526-85de-46aa-8c03-2cfd095b9795',2,'7','B','2019-01-01',NULL,'B',NULL,NULL,'A','Texto Qualquer'),
+('1d668b37-18d0-4f5a-b3ec-7e10bd6b29f6',2,'8','C','2020-01-01',NULL,'C',NULL,NULL,'A','Texto Qualquer');
